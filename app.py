49777f6e9e915682b80af12e0dc3488c2141782f
Johana070500 <johana.zuniga7679@alumnos.udg.mx>
2023-10-13 00:14:01 -0600

    x

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask,  render_template, request, redirect, url_for, session 
+from flask_mysqldb import MySQL,MySQLdb 
+from os import path 
+from notifypy import Notify
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'app_iam'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template("contenido.html")    
 
